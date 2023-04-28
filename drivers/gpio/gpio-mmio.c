e4a9cb0c99b6f3d03c5418bdc952e8040b6dccc9
Dominique Brochu <DominiqueBrochu@Eaton.com>
2023-04-28 12:56:04 +0000

    WI #81056 - Kernel update to kirkstone

--- drivers/gpio/gpio-mmio.c
+++ drivers/gpio/gpio-mmio.c
@@ -222,6 +222,11 @@ static void bgpio_set(struct gpio_chip *gc, unsigned int gpio, int val)
 
 	spin_lock_irqsave(&gc->bgpio_lock, flags);
 
+	if (!(flags & BGPIOF_UNREADABLE_REG_SET))
+		gc->bgpio_data = gc->read_reg(gc->reg_set);
+	else 
+		gc->bgpio_data = gc->read_reg(gc->reg_dat);
+
 	if (val)
 		gc->bgpio_data |= mask;
 	else
@@ -249,6 +254,11 @@ static void bgpio_set_set(struct gpio_chip *gc, unsigned int gpio, int val)
 	unsigned long flags;
 
 	spin_lock_irqsave(&gc->bgpio_lock, flags);
+
+	if (!(flags & BGPIOF_UNREADABLE_REG_SET))
+		gc->bgpio_data = gc->read_reg(gc->reg_set);
+	else 
+		gc->bgpio_data = gc->read_reg(gc->reg_dat);
 
 	if (val)
 		gc->bgpio_data |= mask;
@@ -289,6 +299,11 @@ static void bgpio_set_multiple_single_reg(struct gpio_chip *gc,
 	spin_lock_irqsave(&gc->bgpio_lock, flags);
 
 	bgpio_multiple_get_masks(gc, mask, bits, &set_mask, &clear_mask);
+
+	if (gc->set == bgpio_set_set &&	!(flags & BGPIOF_UNREADABLE_REG_SET))
+		gc->bgpio_data = gc->read_reg(gc->reg_set);
+	else 
+		gc->bgpio_data = gc->read_reg(gc->reg_dat);
 
 	gc->bgpio_data |= set_mask;
 	gc->bgpio_data &= ~clear_mask;
@@ -349,6 +364,11 @@ static int bgpio_dir_in(struct gpio_chip *gc, unsigned int gpio)
 
 	spin_lock_irqsave(&gc->bgpio_lock, flags);
 
+	if (gc->reg_dir_out)
+		gc->bgpio_dir = gc->read_reg(gc->reg_dir_out);
+	else if (gc->reg_dir_in)
+		gc->bgpio_dir = ~gc->read_reg(gc->reg_dir_in);
+
 	gc->bgpio_dir &= ~bgpio_line2mask(gc, gpio);
 
 	if (gc->reg_dir_in)
@@ -388,6 +408,11 @@ static void bgpio_dir_out(struct gpio_chip *gc, unsigned int gpio, int val)
 	unsigned long flags;
 
 	spin_lock_irqsave(&gc->bgpio_lock, flags);
+
+	if (gc->reg_dir_out)
+		gc->bgpio_dir = gc->read_reg(gc->reg_dir_out);
+	else if (gc->reg_dir_in)
+		gc->bgpio_dir = ~gc->read_reg(gc->reg_dir_in);
 
 	gc->bgpio_dir |= bgpio_line2mask(gc, gpio);
 
